9c38832b34d1f22ff355f50f26ee567a23ad329a
yexuchen <245849691@qq.com>
2022-03-14 13:38:33 +0800

    deploy

---
+++ precache-manifest.591bd6c4fbf293273417bae13ff88a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c69ae8101e0747e72839cc15809092",
+    "url": "/MoneyCat-React-Website/index.html"
+  },
+  {
+    "revision": "c666798fb8d40441543a",
+    "url": "/MoneyCat-React-Website/static/css/main.378de560.chunk.css"
+  },
+  {
+    "revision": "4dc83237514b396dec16",
+    "url": "/MoneyCat-React-Website/static/js/2.af4007da.chunk.js"
+  },
+  {
+    "revision": "fc8b115d4064d6797b6def218d457695",
+    "url": "/MoneyCat-React-Website/static/js/2.af4007da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c666798fb8d40441543a",
+    "url": "/MoneyCat-React-Website/static/js/main.d508b08f.chunk.js"
+  },
+  {
+    "revision": "500d69dec306908fa37d",
+    "url": "/MoneyCat-React-Website/static/js/runtime-main.9672f86c.js"
+  }
+]);
